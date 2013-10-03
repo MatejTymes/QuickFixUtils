23805cc2c11be7a0b53a774a65858a7e68b89739
MatejTymes <mtymes@>
2013-10-03 16:23:37 +0200

    mtymes: shortened test

--- src/test/java/com/matejtymes/qfu/matcher/FIXMessageMatcherTest.java
+++ src/test/java/com/matejtymes/qfu/matcher/FIXMessageMatcherTest.java
@@ -2,6 +2,7 @@ package com.matejtymes.qfu.matcher;
 
 import org.junit.Test;
 import quickfix.field.*;
+import quickfix.fix44.ExecutionReport;
 import quickfix.fix44.Message;
 import quickfix.fix44.NewOrderList;
 import quickfix.fix44.NewOrderSingle;
@@ -11,7 +12,7 @@ import java.util.Date;
 import static com.matejtymes.qfu.matcher.FIXMessageMatcher.isFixMessage;
 import static com.matejtymes.qfu.matcher.Group.group;
 import static com.matejtymes.qfu.matcher.Header.header;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 /**
@@ -21,109 +22,55 @@ import static org.junit.Assert.assertThat;
 public class FIXMessageMatcherTest {
 
     @Test
-    public void shouldMatchFIXMessageOfTheSameType() {
+    public void shouldMatchBasedOnFIXMessageType() {
         // Given
-        Message message = new NewOrderSingle();
+        NewOrderSingle message = new NewOrderSingle();
 
-        // When
-        FIXMessageMatcher matcher = isFixMessage().ofType(NewOrderSingle.class);
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(true));
+        // When & Then
+        assertThat(message, isFixMessage(NewOrderSingle.class));
+        assertThat(message, isFixMessage().ofType(NewOrderSingle.class));
+        assertThat(message, not(isFixMessage(ExecutionReport.class)));
+        assertThat(message, not(isFixMessage().ofType(ExecutionReport.class)));
     }
 
     @Test
-    public void shouldNotMatchFIXMessageOfDifferentType() {
-        // Given
-        Message message = new NewOrderSingle();
-
-        // When
-        FIXMessageMatcher matcher = isFixMessage().ofType(NewOrderList.class);
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(false));
-    }
-
-    @Test
-    public void shouldMatchFIXMessageWithSameStringValue() {
+    public void shouldMatchFIXMessageBasedOnStringValue() {
         // Given
         NewOrderSingle message = new NewOrderSingle();
         message.set(new ClOrdID("clOrdId-123"));
 
-        // When
-        FIXMessageMatcher matcher = isFixMessage().with(ClOrdID.FIELD, "clOrdId-123");
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(true));
+        // When & Then
+        assertThat(message, isFixMessage().with(ClOrdID.FIELD, "clOrdId-123"));
+        assertThat(message, not(isFixMessage().with(ClOrdID.FIELD, "clOrdId-456")));
     }
 
     @Test
-    public void shouldNotMatchFIXMessageWithDifferentStringValue() {
-        // Given
-        NewOrderSingle message = new NewOrderSingle();
-        message.set(new ClOrdID("clOrdId-123"));
-
-        // When
-        FIXMessageMatcher matcher = isFixMessage().with(ClOrdID.FIELD, "clOdrId-456");
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(false));
-    }
-
-    @Test
-    public void shouldMatchFIXMessageWithSameHeaderStringValue() {
+    public void shouldMatchFIXMessageBasedOnStringHeaderValue() {
         // Given
         NewOrderSingle message = new NewOrderSingle();
         message.getHeader().setField(new SenderSubID("senderSubId-123"));
 
-        // When
-        FIXMessageMatcher matcher = isFixMessage().withHeader(SenderSubID.FIELD, "senderSubId-123");
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(true));
+        // When & Then
+        assertThat(message, isFixMessage().withHeader(SenderSubID.FIELD, "senderSubId-123"));
+        assertThat(message, isFixMessage().with(header().with(SenderSubID.FIELD, "senderSubId-123")));
+        assertThat(message, not(isFixMessage().withHeader(SenderSubID.FIELD, "senderSubId-456")));
+        assertThat(message, not(isFixMessage().with(header().with(SenderSubID.FIELD, "senderSubId-456"))));
     }
 
     @Test
-    public void shouldNotMatchFIXMessageWithDifferentHeaderStringValue() {
-        // Given
-        NewOrderSingle message = new NewOrderSingle();
-        message.getHeader().setField(new SenderSubID("senderSubId-123"));
-
-        // When
-        FIXMessageMatcher matcher = isFixMessage().withHeader(SenderSubID.FIELD, "senderSubId-456");
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(false));
-    }
-
-    @Test
-    public void shouldMatchFIXMessageWithSameGroupStringValue() {
+    public void shouldMatchFIXMessageBasedOnStringGroupValue() {
         // Given
         Message message = new NewOrderList();
+
         NewOrderList.NoOrders group = new NewOrderList.NoOrders();
         group.set(new ClOrdID("clOrdId-123"));
         message.addGroup(group);
 
-        // When
-        FIXMessageMatcher matcher = isFixMessage().withGroup(1, NoOrders.FIELD, ClOrdID.FIELD, "clOrdId-123");
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(true));
-    }
-
-    @Test
-    public void shouldNotMatchFIXMessageWithDifferentGroupStringValue() {
-        // Given
-        Message message = new NewOrderList();
-        NewOrderList.NoOrders group = new NewOrderList.NoOrders();
-        group.set(new ClOrdID("clOrdId-123"));
-        message.addGroup(group);
-
-        // When
-        FIXMessageMatcher matcher = isFixMessage().withGroup(1, NoOrders.FIELD, ClOrdID.FIELD, "clOrdId-456");
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(false));
+        // When & Then
+        assertThat(message, isFixMessage().withGroup(1, NoOrders.FIELD, ClOrdID.FIELD, "clOrdId-123"));
+        assertThat(message, isFixMessage().with(group(1, NoOrders.FIELD).with(ClOrdID.FIELD, "clOrdId-123")));
+        assertThat(message, not(isFixMessage().withGroup(1, NoOrders.FIELD, ClOrdID.FIELD, "clOrdId-456")));
+        assertThat(message, not(isFixMessage().with(group(1, NoOrders.FIELD).with(ClOrdID.FIELD, "clOrdId-456"))));
     }
 
     @Test
@@ -142,8 +89,8 @@ public class FIXMessageMatcherTest {
         group.set(new OrdType(OrdType.FOREX_MARKET));
         message.addGroup(group);
 
-        // When
-        FIXMessageMatcher matcher = isFixMessage(NewOrderList.class)
+        // When & Then
+        assertThat(message, isFixMessage(NewOrderList.class)
                 .withHeader(SenderSubID.FIELD, "senderSubId-123")
                 .with(ListID.FIELD, "listId-123")
                 .with(BidType.FIELD, BidType.NON_DISCLOSED)
@@ -151,30 +98,10 @@ public class FIXMessageMatcherTest {
                 .withGroup(1, NoOrders.FIELD, ClOrdID.FIELD, "clOrdId-123")
                 .withGroup(1, NoOrders.FIELD, Side.FIELD, Side.SELL)
                 .withGroup(1, NoOrders.FIELD, TransactTime.FIELD, now)
-                .withGroup(1, NoOrders.FIELD, OrdType.FIELD, OrdType.FOREX_MARKET);
+                .withGroup(1, NoOrders.FIELD, OrdType.FIELD, OrdType.FOREX_MARKET)
+        );
 
-        // Then
-        assertThat(matcher.matchesSafely(message), is(true));
-    }
-
-    @Test
-    public void shouldMatchFIXMessageWithComplexMatchCriteriaV2() {
-        // Given
-        Date now = new Date();
-        NewOrderList message = new NewOrderList();
-        message.getHeader().setField(new SenderSubID("senderSubId-123"));
-        message.set(new ListID("listId-123"));
-        message.set(new BidType(BidType.NON_DISCLOSED));
-        message.set(new TotNoOrders(1));
-        NewOrderList.NoOrders group = new NewOrderList.NoOrders();
-        group.set(new ClOrdID("clOrdId-123"));
-        group.set(new Side(Side.SELL));
-        group.set(new TransactTime(now));
-        group.set(new OrdType(OrdType.FOREX_MARKET));
-        message.addGroup(group);
-
-        // When
-        FIXMessageMatcher matcher = isFixMessage(NewOrderList.class)
+        assertThat(message, isFixMessage(NewOrderList.class)
                 .with(header().with(SenderSubID.FIELD, "senderSubId-123"))
                 .with(ListID.FIELD, "listId-123")
                 .with(BidType.FIELD, BidType.NON_DISCLOSED)
@@ -184,9 +111,7 @@ public class FIXMessageMatcherTest {
                         .with(Side.FIELD, Side.SELL)
                         .with(TransactTime.FIELD, now)
                         .with(OrdType.FIELD, OrdType.FOREX_MARKET)
-                );
-
-        // Then
-        assertThat(matcher.matchesSafely(message), is(true));
+                )
+        );
     }
 }
